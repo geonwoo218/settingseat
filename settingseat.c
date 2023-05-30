ef3b253202f18a047e7998efee89491f1c20f45a
geonwoo218 <ekw218@naver.com>
2023-05-30 21:48:17 +0900

    fileUplode
    파일업로드

---
+++ settingseat.c
@@ -0,0 +1,42 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+
+int main(){
+    int studentcount;
+    int student[100]={0,};
+    int seat[100]={0,};
+	int i,j;
+    printf("학생 수를 입력하세요: ");
+    scanf("%d",&studentcount);
+
+    if(studentcount>100){
+        printf("최대 학생 수는 100명입니다.\n");
+        return 1;
+    }
+
+    //학번 생성
+    for(i = 0 ; i < studentcount ; i++){
+        student[i] = 20501 +i;
+    }
+
+     //rand 함수를 위한 시드값 설정
+     srand(time(NULL));
+
+     //자리 랜덤 배치
+     for(i = 0;i<studentcount;i++){
+     	seat[i]=rand()%studentcount+1;
+     	for(j = 0;j<i;j++){
+     		if(seat[i] == seat[j])
+     		i--;
+		 }
+	 }
+
+     printf("자리 배치 결과: \n");
+     for(i=0;i<studentcount;i++){
+        printf("학번: %d \t자리: %2d번\n",student[i],seat[i]);
+     }
+
+     return 0;
+}
+
